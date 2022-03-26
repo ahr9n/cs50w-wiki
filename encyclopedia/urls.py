00500dd834bcf060b49808146e7356cff9cae119
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-03-26 20:26:30 +0200

    Add all paths of encyclopedia in `urlpatterns`

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("random", views.random, name="random"),
+    path("create", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit"),
 ]
