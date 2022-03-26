7d7038730d453e764977bab288d2320dfd7a2a3e
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-03-26 18:06:45 +0200

    Add entry page [view content & error page]

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -8,3 +9,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is not None:
+        entry_html = markdown(content)
+    else:
+        entry_html = "<h1>Error: Page Not Found</h1>"
+
+    return render(request, "encyclopedia/entry.html", {"title": title, "entry": entry_html})
